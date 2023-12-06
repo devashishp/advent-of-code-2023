21fe5742d38ecf0da8e1d8ba1aff9ec5cf633635
Dev <dpuranda@ucsc.edu>
2023-12-05 23:49:37 -0800

    Day 5 Done!

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::{collections::HashMap, fs};
 
 #[derive(Debug, Default, Clone, Copy)]
@@ -7,14 +8,16 @@ struct Page {
     range: usize,
 }
 impl Page {
-    fn new(source: usize, destination: usize,range: usize) -> Self {
-        Self{
-            source,destination,range, 
+    fn new(source: usize, destination: usize, range: usize) -> Self {
+        Self {
+            source,
+            destination,
+            range,
         }
     }
-    
 }
 
+/// Index has all the types
 #[derive(Debug, PartialEq, PartialOrd, Eq, Hash, Clone, Copy)]
 enum Index {
     None,
@@ -28,21 +31,25 @@ enum Index {
 }
 
 fn main() {
-    let input = fs::read_to_string("input/mini.txt").expect(
+    let input = fs::read_to_string("input/input.txt").expect(
         "Unable to find
     input",
     );
-    let mut almanac: HashMap<Index,Vec<Page>> = HashMap::new();
+    let mut almanac: HashMap<Index, Vec<Page>> = HashMap::new();
     let mut seeds: Vec<usize> = Vec::new();
     let mut seeds_part_two: Vec<usize> = Vec::new();
-    let mut guide:Index = Index::None;
+    let mut guide: Index = Index::None;
     for line in input.lines() {
         match line {
             x if x.contains("seeds") => {
-                seeds.extend(x.split_ascii_whitespace().filter_map(|f| f.parse::<usize>().ok()).collect::<Vec<usize>>());
-            },
+                seeds.extend(
+                    x.split_ascii_whitespace()
+                        .filter_map(|f| f.parse::<usize>().ok())
+                        .collect::<Vec<usize>>(),
+                );
+            }
             x if x.contains("-to-") => {
-                guide =  match x.split(' ').next().unwrap() {
+                guide = match x.split(' ').next().unwrap() {
                     "seed-to-soil" => Index::SeedToSoil,
                     "soil-to-fertilizer" => Index::SoilToFertilizer,
                     "fertilizer-to-water" => Index::FertilizerToWater,
@@ -51,15 +58,18 @@ fn main() {
                     "temperature-to-humidity" => Index::TemperatureToHumidity,
                     "humidity-to-location" => Index::HumidityToLocation,
                     _ => panic!("Error did not match the string"),
-
                 };
-            },
+            }
             x if x.split_ascii_whitespace().collect::<Vec<_>>().len() == 3 => {
-                let nums = x.split_ascii_whitespace().map(|f|
-                f.parse::<usize>().unwrap()).collect::<Vec<usize>>();
-                let tuple:Page = Page::new(nums[1], nums[0], nums[2]);
-                almanac.entry(guide).and_modify(|f|
-                f.push(tuple)).or_insert(vec![tuple]);
+                let temp_vec = x
+                    .split_ascii_whitespace()
+                    .map(|f| f.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>();
+                let tuple: Page = Page::new(temp_vec[1], temp_vec[0], temp_vec[2]);
+                almanac
+                    .entry(guide)
+                    .and_modify(|f| f.push(tuple))
+                    .or_insert(vec![tuple]);
             }
             _ => continue,
         };
@@ -75,42 +85,38 @@ fn main() {
     println!("Second part is {second}");
 }
 
-fn first_part(almanac: &HashMap<Index,Vec<Page>>, seeds: &Vec<usize>) -> usize {
-    let mut smallest_location = usize::MAX;
-    for seed in seeds {
-        let mut location = seed.clone();
-        for method in [&Index::SeedToSoil, &Index::SoilToFertilizer,
-        &Index::FertilizerToWater, &Index::WaterToLight,
-        &Index::LightToTemperature, &Index::TemperatureToHumidity,
-        &Index::HumidityToLocation] {
-            location = get_next_number(almanac, method, location);
-        }
-        if location < smallest_location {
-            smallest_location = location;
-        }
-    }
-   smallest_location 
+fn first_part(almanac: &HashMap<Index, Vec<Page>>, seeds: &Vec<usize>) -> usize {
+    seeds
+        .par_iter()
+        .map(|seed| {
+            let mut location = *seed;
+            for method in [
+                &Index::SeedToSoil,
+                &Index::SoilToFertilizer,
+                &Index::FertilizerToWater,
+                &Index::WaterToLight,
+                &Index::LightToTemperature,
+                &Index::TemperatureToHumidity,
+                &Index::HumidityToLocation,
+            ] {
+                location = get_next_number(almanac, method, location);
+            }
+            location
+        })
+        .min()
+        .unwrap()
 }
 
-fn get_next_number(almanac: &HashMap<Index,Vec<Page>>, index:&Index, seed: usize) -> usize {
-    // Cases : soil is not mapped: return input, soil is explicitely mapped: mapped
+fn get_next_number(almanac: &HashMap<Index, Vec<Page>>, index: &Index, seed: usize) -> usize {
+    // Cases : soil is not mapped: return input, soil is explicitly mapped: mapped
     let current_page = almanac.get(index).unwrap();
-   // println!("{:?}",current_page);
-    let chosen = current_page.iter().filter(|f|
-    (f.source..(f.source+f.range)).contains(&seed)).collect::<Vec<_>>();
-    if chosen.is_empty(){
-        return seed
-    } else {
+    // println!("{:?}",current_page);
+    let chosen = current_page
+        .iter()
+        .filter(|f| (f.source..(f.source + f.range)).contains(&seed))
+        .collect::<Vec<_>>();
+    if !chosen.is_empty() {
         return (seed - chosen[0].source) + chosen[0].destination;
-
     }
-    
+    seed
 }
-
-//fn first_part(&input) {
-//    todo!()
-//}
-//
-//fn second_part(&input) {
-//    todo!()
-//}
